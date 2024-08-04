54ddda8cd03aa7c8a420a54b25c0098b5d6de642
WYCTSTF <996098413@qq.com>
2024-08-04 23:08:48 +0800

    颜色主题，写的应该是有问题的，先凑合用

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'NAlexPear/Spacegray.nvim'
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    priority = 52,
+    config = function()
+      pcall(vim.cmd, "colorscheme github_light")
+      pcall(vim.cmd, "highlight Normal guifg=#000000 guibg=#FFFFFF")
+      -- pcall(vim.cmd, "highlight Normal guibg=NONE ctermbg=NONE")
+    end
+  }
+}
