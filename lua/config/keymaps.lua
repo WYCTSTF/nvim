02c701acb67382f036ac21d4105dfa90c0962173
WYCTSTF <996098413@qq.com>
2024-08-04 23:01:23 +0800

    keyboard mappings.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,24 @@
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+
+map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
+map("n", "s,", ":vertical resize -20<CR>", opt)
+map("n", "s.", ":vertical resize +20<CR>", opt)
+
+map("n", "sj", ":resize +10<CR>", opt)
+map("n", "sk", ":resize -10<CR>", opt)
+map("n", "<C-Down>", ":resize +2<CR>", opt)
+map("n", "<C-Up>", ":resize -2<CR>", opt)
+
+map("n", "<leader>wq", ":wqa<CR>", opt)
+
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+map("n", "<M-h>", "<C-w>h", opt)
+map("n", "<M-j>", "<C-w>j", opt)
+map("n", "<M-k>", "<C-w>k", opt)
+map("n", "<M-l>", "<C-w>l", opt)
