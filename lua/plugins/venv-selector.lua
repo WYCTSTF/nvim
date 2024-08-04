6880b3cd08fe093c713085a7d11b64971df5ebba
WYCTSTF <996098413@qq.com>
2024-08-04 23:01:06 +0800

    python virtual enviroment selector, integrated with lsp.

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,24 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  ft = 'python',
+  dependencies = {
+    "neovim/nvim-lspconfig", 
+    "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  config = function()
+    require("venv-selector").setup( {
+      settings = {
+        search = {
+          my_envs = {
+            command = "fd python$ /opt/homebrew/Caskroom/miniconda/base"
+          },
+        },
+      },
+    })
+  end,
+  keys = {
+    { "<leader>vs", "<cmd>VenvSelect<cr>" },
+  },
+}
