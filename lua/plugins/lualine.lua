1d66acb20e49c1f83f3ba030a0104820ca62a038
WYCTSTF <996098413@qq.com>
2024-08-07 14:36:57 +0800

    statusline文件改相对路径，方便直接tmux里复制路径，切回终端用

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -27,6 +27,12 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          },
+        },
 
         lualine_x = {
           "encoding",
