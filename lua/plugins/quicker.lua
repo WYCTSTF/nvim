d5a7a1e71eae0bc34c16249ed371b74336a94d51
WYCTSTF <996098413@qq.com>
2024-08-08 15:24:16 +0800

    quicker.nvim 之后看时间，等成熟了再配置

--- lua/plugins/quicker.lua
+++ lua/plugins/quicker.lua
@@ -1,6 +1,11 @@
 return {
   'stevearc/quicker.nvim',
-  ---@module "quicker"
-  ---@type quicker.SetupOptions
   opts = {},
+  config = function()
+    vim.keymap.set("n", "<leader>q", function()
+      require('quicker').toggle()
+    end, {
+      desc = "Toggle quickfix",
+    })
+  end,
 }
