2d0b8ed06b88554c4b4c2d85fbb4ae4c07be6913
WYCTSTF <996098413@qq.com>
2024-09-14 16:58:16 +0800

    keybinding

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -64,5 +64,13 @@ return {
       pattern = [[\b(KEYWORDS):]], -- ripgrep regex
       -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
-  }
+  },
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+    { "<leader>tt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+    { "<leader>tl", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+  },
 }
