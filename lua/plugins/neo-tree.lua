bb7ed892a84cf66369ad192dfb06b807c0c39163
WYCTSTF <996098413@qq.com>
2024-09-14 11:52:58 +0800

    fix.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,8 +11,6 @@ return {
     },
   },
   init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
     vim.api.nvim_create_autocmd("BufEnter", {
       group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
       desc = "Start Neo-tree with directory",
@@ -20,36 +18,45 @@ return {
       callback = function()
         if package.loaded["neo-tree"] then
           return
-        else
-          local stats = vim.uv.fs_stat(vim.fn.argv(0))
-          if stats and stats.type == "directory" then
-            require("neo-tree")
+        end
+
+        local path = vim.fn.argv(0)
+        if path == "" then
+          path = vim.fn.getcwd()
+        end
+
+        local stats = vim.uv.fs_stat(path)
+        if stats and stats.type == "directory" then
+          local status, neo_tree = pcall(require, "neo-tree")
+          if not status then
+            vim.notify("无法加载 neo-tree: " .. neo_tree, vim.log.levels.ERROR)
+            return
           end
+          neo_tree.setup({
+            sources = { "filesystem", "buffers", "git_status" },
+            open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+            filesystem = {
+              bind_to_cwd = false,
+              follow_current_file = { enabled = true },
+              use_libuv_file_watcher = true,
+            },
+            default_component_configs = {
+              indent = {
+                with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander",
+              },
+              git_status = {
+                symbols = {
+                  unstaged = "󰄱",
+                  staged = "󰱒",
+                },
+              },
+            },
+          })
         end
       end,
     })
   end,
-  opts = {
-    sources = { "filesystem", "buffers", "git_status" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-      git_status = {
-        symbols = {
-          unstaged = "󰄱",
-          staged = "󰱒",
-        },
-      },
-    },
-  },
 }
