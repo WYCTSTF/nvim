d7a0d8a291a5e55f1eb5331e16ebc20ca0d17bb8
WYCTSTF <996098413@qq.com>
2024-09-19 10:51:49 +0800

    todo-comment.nvim取消懒加载

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  lazy = false,
   opts = {
     signs = true, -- show icons in the signs column
     sign_priority = 8, -- sign priority
