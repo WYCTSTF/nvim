bc948db0b13d36c56df1dd9f849c2a880b8c4623
WYCTSTF <996098413@qq.com>
2024-10-08 14:45:28 +0800

    。

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
     'projekt0n/github-nvim-theme',
     priority = 52,
     config = function()
-      pcall(vim.cmd, "colorscheme github_dark")
+      pcall(vim.cmd, "colorscheme spacegray")
       -- pcall(vim.cmd, "highlight Normal guifg=#000000 guibg=#FFFFFF")
       -- pcall(vim.cmd, "highlight Normal guibg=NONE ctermbg=NONE")
     end
