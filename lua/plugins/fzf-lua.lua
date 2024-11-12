ec280b4cbe1e61a5d972a63ea30e2c5f56f0f822
WYCTSTF <996098413@qq.com>
2024-11-12 22:20:20 +0800

    fzflua support

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -2,6 +2,9 @@ return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    {"<C-p>", ":FzfLua<CR>" },
+  },
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({})
