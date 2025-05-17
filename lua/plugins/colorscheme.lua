0247a5ee65077f78df25eef192b8e56d2a5fe793
WYCTSTF <996098413@qq.com>
2025-05-17 20:35:21 +0800

    bufferline透明配置，更改适用的spacegray主题

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,10 @@ return {
   },
   {
     'projekt0n/github-nvim-theme',
-    priority = 52,
+    priority = 10,
+    after = "xiyaowong/transparent.nvim",
     config = function()
-      pcall(vim.cmd, "colorscheme github_dark")
+      pcall(vim.cmd, "colorscheme spacegray")
       -- pcall(vim.cmd, "highlight Normal guifg=#000000 guibg=#FFFFFF")
       -- pcall(vim.cmd, "highlight Normal guibg=NONE ctermbg=NONE")
     end
