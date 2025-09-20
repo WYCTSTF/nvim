8a2c0bf5eef61e3273eae0477271c18b135ef626
WYCTSTF <996098413@qq.com>
2025-09-20 16:57:04 +0800

    format.

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -6,7 +6,8 @@ return {
     "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
-  branch = "regexp", -- This is the regexp branch, use this for the new version
+  -- branch = "regexp", -- This is the regexp branch, use this for the new version
+  branch = "main",
   config = function()
     require("venv-selector").setup( {
       settings = {
