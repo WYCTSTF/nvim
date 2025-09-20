cd53ff041d3b74211ade444e7d1c6a1a85c71e62
WYCTSTF <996098413@qq.com>
2025-09-20 16:57:15 +0800

    include relative header

--- lua/plugins/competitest.lua
+++ lua/plugins/competitest.lua
@@ -1,12 +1,25 @@
 return {
   'xeluxee/competitest.nvim',
   lazy = false,
-  dependencies = 'MunifTanjim/nui.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
   config = function()
+    local function get_header_include()
+      -- 获取当前文件目录并拼接 '/header'
+      local fname = vim.fn.expand("%:p")
+      local dir = vim.fn.fnamemodify(fname, ":h")
+      return "-I" .. dir .. "/header"
+    end
+
     require('competitest').setup({
       compile_command = {
-        c = { exec = "clang", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)", "-std=c++17", "-DLOCAL", "-I/home/syh/solution/header"} },
-        cpp = { exec = "clang++", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)", "-std=c++17", "-DLOCAL", "-I/home/syh/solution/header" } },
+        c = {
+          exec = "clang",
+          args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)", "-std=c++17", "-DLOCAL", get_header_include() }
+        },
+        cpp = {
+          exec = "clang++",
+          args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)", "-std=c++17", "-DLOCAL", get_header_include() }
+        },
       },
     })
   end,
